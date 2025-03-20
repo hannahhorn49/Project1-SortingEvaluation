3543ea6f2e0f999c35e9c271e47960aa833deb4f
Hannah Horn <hhorn@Hannahs-MacBook-Air-9.local>
2025-03-20 11:35:13 -0700

    start evaluator class

---
+++ evaluator.hpp
@@ -0,0 +1,26 @@
+#ifndef EVALUATOR_HPP
+#define EVALUATOR_HPP
+
+#include <vector>
+#include <string>
+#include <doubly_linked_list.hpp>
+#include <vector_sorter.hpp>
+#include <chrono> // this is a way we can time our sorts
+
+class Evaluator
+{
+private:
+    // these are the member variables to store the evaluation cases and timing data
+    std::vector<std::vector<int>> testVectors;
+    std::vector<DoublyLinkedList<int>> testLists;
+    std::vector<std::vector<double>> timingData;
+
+public:
+    void Ingest(const std::string &filePath);
+    void MergeComparison();
+    void QuickComparison();
+    void InsertionComparison();
+    void Evaluate() const;
+};
+
+#endif
