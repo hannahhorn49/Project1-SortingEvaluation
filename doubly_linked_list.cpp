a466f04f970528691bf1b75a967f22d17b8d5a84
Fionacallahan <ficallahan17@gmail.com>
2025-03-23 12:09:16 -0700

    need to figure out test class

--- doubly_linked_list.cpp
+++ doubly_linked_list.cpp
@@ -1,5 +1,120 @@
 #include "doubly_linked_list.hpp"
 
-DLLNode* DoublyLinkedList::merge_sort(DLLNode *head){
-    //need to implement 
+DLLNode::DLLNode(int val){
+    value = val;
+    prev = nullptr;
+    next = nullptr;
+}
+
+DLLNode::DLLNode(int val, DLLNode *p, DLLNode *n)
+{
+    value = val;
+    prev = p;
+    next = n;
+}
+
+DoublyLinkedList::DoublyLinkedList()
+{
+    tail = nullptr;
+    head = nullptr;
+}
+
+//this function is here to help with testing 
+void DoublyLinkedList::push_back(int v)
+{
+    if (head == nullptr)
+    {
+        DLLNode *temp = new DLLNode(v);
+        head = temp;
+        
+    }
+    else if(head->next->next == nullptr)
+    {
+        DLLNode *temp = new DLLNode(v, head, nullptr);
+        tail = temp;
+        head->next = temp;
+        
+    }
+    else{
+        DLLNode *iter = tail;
+        DLLNode* temp2 = new DLLNode(v, iter, nullptr);
+        iter->next = temp2;
+        tail = temp2;
+        
+    }
+}
+
+DLLNode* DoublyLinkedList::DLL_merge_sort(DLLNode *head){
+    if(head == nullptr)
+    {
+        //first case: list is empty
+        return head;
+    }
+    else if(head->next == nullptr)
+    {
+        //next case: list has one element 
+        return head;
+    }
+    else{
+        DLLNode *second = DLL_merge_sort_helper(head);
+
+        head = DLL_merge_sort(head);
+        second = DLL_merge_sort(second);
+
+        return merge(head, second);
+    }
+}
+
+DLLNode* DoublyLinkedList::DLL_merge_sort_helper(DLLNode *dllnode){
+    DLLNode *two = dllnode;
+    DLLNode *one = dllnode;
+
+    //looked at AI on how to find middle of list since no middle values 
+    while(two != nullptr && two->next != nullptr && two->next->next != nullptr)
+    {
+        two = two->next->next;
+        one = one->next;
+    }
+
+    //this splits the list into two 
+    DLLNode *temp = one->next;
+    one->next = nullptr;
+    if(temp != nullptr)
+    {
+        temp->prev = nullptr;
+    }
+    return temp;
+
+}
+
+DLLNode* DoublyLinkedList::merge(DLLNode *one_node, DLLNode *two_node){
+    if(one_node == nullptr)
+    {
+        return two_node;
+    }
+    else if(two_node == nullptr)
+    {
+        return one_node;
+    }
+    else{
+        if(one_node->value < two_node->value)
+        {
+            one_node->next = merge(one_node->next, two_node);
+            if(one_node->next != nullptr)
+            {
+                one_node->next->prev = one_node;
+            }
+            one_node->prev = nullptr;
+            return one_node;
+        }
+        else{
+            two_node->next = merge(one_node, two_node->next);
+            if(two_node->next != nullptr)
+            {
+                two_node->prev->next = two_node;
+            }
+            two_node->prev = nullptr;
+            return two_node;
+        }
+    }
 }
--- doubly_linked_list.hpp
+++ doubly_linked_list.hpp
@@ -1,34 +1,13 @@
 #ifndef DLL_SORTER_HPP
 #define DLL_SORTER_HPP
 
-class DoublyLinkedList{
-    private:
-        DLLNode *head;
-        DLLNode *tail;
-        //basic outline: will change parameters/names and such 
-        DLLNode* merge_sort(DLLNode *dllnode);
-        void quick_sort(DLLNode *head);
-        DLLNode* merge(DLLNode *one_node, DLLNode *two_node);
-
-
-    public:
-        //basic outline: will fix them further down the road
-        DLLNode* merge_sort(DLLNode *head);
-        void quick_sort();
-        void insertion_sort(DLLNode *head);
-
-        DLLNode *get_head(){ return head;}
-        DLLNode *get_tail(){ return tail;}
-
-};
-
 class DLLNode{
     public: 
         int value;
         DLLNode* prev;
         DLLNode* next;
 
-        DLLNode();
+        DLLNode(int val);
         DLLNode(int v, DLLNode* n, DLLNode *p);
         //do not think we will need a constructor
     
@@ -37,5 +16,34 @@ class DLLNode{
 
 
 };
+
+class DoublyLinkedList{
+    private:
+        DLLNode *head;
+        DLLNode *tail;
+        //basic outline: will change parameters/names and such 
+        DLLNode* DLL_merge_sort_helper(DLLNode *dllnode);
+        void quick_sort(DLLNode *head);
+        DLLNode* merge(DLLNode *one_node, DLLNode *two_node);
+
+
+
+    public:
+        //basic outline: will fix them further down the road
+        DoublyLinkedList();
+        DLLNode* DLL_merge_sort(DLLNode *head);
+        void quick_sort();
+        void insertion_sort(DLLNode *head);
+
+        //to help with testing: 
+        DLLNode *get_head(){ return head;}
+        DLLNode *get_tail(){ return tail;}
+
+        void push_back(int v);
+
+
+
+};
+
 
 #endif
